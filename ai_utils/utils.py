9eb484150cd113da52ef70a525705fc3f9cde657
Vinayak-HUB1 <dumbre.vinayak@gmail.com>
2022-08-08 08:54:28 +0530

    utils modified

--- ai_utils/utils.py
+++ ai_utils/utils.py
@@ -1,13 +1,13 @@
 import base64
 
-
+# it will convert the base64 string to image.
 def decodeImage(imgstring, fileName):
     imgdata = base64.b64decode(imgstring)
     with open(fileName, 'wb') as f:
         f.write(imgdata)
         f.close()
 
-
+# it will convert image into a base64 string.
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
